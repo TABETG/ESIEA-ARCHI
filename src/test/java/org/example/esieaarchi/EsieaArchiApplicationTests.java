451f733a685818a48d592789031a94e68af4f679
TABET <b_tabet@esi.dz>
2026-03-12 22:20:13 +0100

    First commit

---
+++ src/test/java/org/example/esieaarchi/EsieaArchiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.esieaarchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsieaArchiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
